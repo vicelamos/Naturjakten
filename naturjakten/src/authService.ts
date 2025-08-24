9e334869782d04d6f2acfbbe580f20a8e69f6fea
viktor <vice.andre@gmail.com>
2025-08-24 13:44:12 +0200

    första

--- naturjakten/src/authService.ts
+++
@@ -1,7 +0,0 @@
-import app from './firebaseConfig';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-export async function login(email: string, password: string) {
-  const auth = getAuth(app);
-  return signInWithEmailAndPassword(auth, email, password);
-}
